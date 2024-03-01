294a7815e9edafaa309de1946d3bacce4f0dcbc3
Ömer Arpacı <91760786+arpaci27@users.noreply.github.com>
2024-03-02 01:36:10 +0300

    Tweetinvi installed and Model created

---
+++ TweetingWithAsp/Controllers/TweetsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TweetingWithAsp.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TweetsController : ControllerBase
+	{
+	}
+}
---
+++ TweetingWithAsp/Model/PostTweetRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace TweetingWithAsp.Model
+{
+	public class PostTweetRequestDto
+	{
+		[JsonProperty("text")]
+		public string Text { get; set; } = string.Empty;
+
+    }
+}
